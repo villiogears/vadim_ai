67fe278bccb484af4111a17ec01bc2ce4947b630
aaa <aaa>
2025-05-22 23:42:14 +0900

    aaaaa

---
+++ app/page.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Image
+          src="/image.svg"
+          alt="Vadim"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2 tracking-[-.01em]">
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+              Start AI Chat
+            </code>
+          </li>
+          <li className="tracking-[-.01em]">
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+              AI Chat
+            </code>
+          </li>
+        </ol>
+
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          
+            <Image
+              src="/image.svg"
+              alt="Logo"
+              width={0}
+              height={0}
+              priority
+            />
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            href="/chat"
+            rel="noopener noreferrer"
+          >
+            Starting Chat
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+}
